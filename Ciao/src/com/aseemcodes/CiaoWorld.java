3a1bc0de69f641be41f43596fbf7c22d7040e373
Aseem Pratap <aseempratap@outlook.com>
2014-02-25 00:04:15 +0530

    Added Hello World :)

---
+++ Ciao/src/com/aseemcodes/CiaoWorld.java
@@ -0,0 +1,10 @@
+package com.aseemcodes;
+
+/**
+ * Created by buddha on 24/2/14.
+ */
+public class CiaoWorld {
+    public static void main(String[] args) {
+        System.out.println("CIAO!!!");
+    }
+}
